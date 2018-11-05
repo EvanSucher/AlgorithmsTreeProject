4e31b9be9c4f981d2b98903d5b9fd25ced849c58
Evan Sucher <evan.sucher@loop.colum.edu>
2018-11-04 22:48:48 -0600

    Created Project
    Initial push, project is largely complete

---
+++ AlgorihmsTree/AlgorihmsTree/Program.cs
@@ -0,0 +1,147 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorihmsTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isRunning = true;
+            string filePath = "names.tab";
+            string[] stringArray;
+            Tree nameTree = new Tree();
+
+            stringArray = System.IO.File.ReadAllLines(filePath);
+
+            nameTree.ConvertStringArrayToTree(stringArray);
+
+
+            do
+            {
+                printMenu();
+                string inputChoice = Console.ReadLine();
+                int choice = 0;
+                int.TryParse(inputChoice, out choice);
+
+                if (choice > 0 && choice < 7) //If the user enters a valid choice
+                {
+                    switch(choice)
+                    {
+                        case 1: //Get Node by ID
+                            {
+                                Console.WriteLine("\nPlease enter the ID of the Node:");
+                                string inputStringID = Console.ReadLine();
+                                int inputID = 0;
+                                int.TryParse(inputStringID, out inputID);
+
+                                if (inputID > 0) //Valid input
+                                {
+                                    Node tempNode = nameTree.GetNodeByID(inputID);
+                                    if (tempNode != null)
+                                    {
+                                        string outputString = tempNode.DisplayNodeInfo();
+                                        Console.WriteLine("\n" + outputString);
+
+                                        waitToContinue();
+                                    }
+                                    else
+                                    {
+                                        errorMessage();
+                                    }
+                                }
+                                else //Invalid Input
+                                {
+                                    errorMessage();
+                                }
+                                break;
+                            }
+                        case 2: //Get Node by Content
+                            {
+                                Console.WriteLine("\nPlease enter the Content/Name of the Node:");
+                                string inputString = Console.ReadLine();
+                                List<Node> outputNodes = nameTree.GetNodesByName(inputString);
+
+                                if(outputNodes.Count() > 0) //At least one matching node in the list
+                                {
+                                    foreach (Node n in outputNodes)
+                                    {
+                                        string outputString = n.DisplayNodeInfo();
+                                        Console.WriteLine("\n" + outputString);
+                                    }
+                                    waitToContinue();
+                                }
+                                else //no matching nodes
+                                {
+                                    errorMessage();
+                                }
+
+                                break;
+                            }
+                        case 3: //Add a Node
+                            {
+
+
+                                break;
+                            }
+                        case 4:
+                            {
+
+
+                                break;
+                            }
+                        case 5:
+                            {
+
+
+                                break;
+                            }
+                        case 6:
+                            {
+                                isRunning = false;
+
+                                break;
+                            }
+                    }
+                        
+                }
+                else
+                {
+                    errorMessage();
+                }
+
+            } while (isRunning);
+
+
+
+            void printMenu()
+            {
+                Console.WriteLine("\n==Enter a number and press enter to select an option==\n");
+                Console.WriteLine("[1] Get Node by ID");
+                Console.WriteLine("[2] Get Node by Content");
+                Console.WriteLine("[3] Add a Node");
+                Console.WriteLine("[4] Move a Node");
+                Console.WriteLine("[5] Delete a Node");
+                Console.WriteLine("[6] Quit");
+            }
+
+            void errorMessage()
+            {
+                Console.WriteLine("\nError: Invalid Option Entered");
+                Console.WriteLine("Please try again to enter an option");
+            }
+
+            void waitToContinue()
+            {
+                Console.WriteLine("\nPress Enter to Continue...");
+                Console.ReadLine();
+            }
+
+        }
+
+        
+    }
+}
---
+++ AlgorihmsTree/AlgorihmsTree/Tree.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorihmsTree
+{
+    class Tree
+    {
+        int globalID = 1; //increments by 1 anytime a new Node is added so that no id numbers are the same
+        List<Node> allNodes = new List<Node>();
+        List<Node> baseNodes = new List<Node>();
+
+        public Node GetNodeByID(int InputID)
+        {
+            foreach (Node n in allNodes)
+            {
+                if (n.id == InputID)
+                {
+                    return n;
+                }
+            }
+            return null;
+        }
+
+        public List<Node> GetNodesByName(string inputString)
+        {
+            List<Node> tempNodeList = new List<Node>();
+            foreach (Node n in allNodes)
+            {
+                if (n.DisplayNodeName() == inputString)
+                {
+                    tempNodeList.Add(n);
+                }
+            }
+            return tempNodeList;
+        }
+
+        public void AddNode(string inputName, int inputID)
+        {
+            
+        }
+
+        // ==============================================================================================
+        //                                  Tree and Node Creation Functions
+        // ==============================================================================================
+        public void ConvertStringArrayToTree(string[] inputArray)
+        {
+            // Takes in string array and defines the baseNodes, and all of the child nodes within
+            FillOutNodeList(inputArray);
+            AssignParentNodes();
+
+
+            foreach (Node n in allNodes)
+            {
+                Console.WriteLine(n.DisplayNodeInfo());
+            }
+
+        }
+
+        public void FillOutNodeList(string[] inputArray)
+        {
+
+            foreach (string s in inputArray)
+            {
+                int tempDepth = CountWhitespacesBeforeString(s);
+                string tempString = RemoveWhitespacesBeforeString(s);
+
+                Node tempNode = new Node(globalID, tempString, tempDepth);
+                allNodes.Add(tempNode);
+                globalID++;
+            }
+        }
+
+        public void AssignParentNodes()
+        {
+            for (int i = 1; i < allNodes.Count(); i++)
+            {
+                if (allNodes[i].depth > 0)
+                {
+                    int parentIndex = FindParent(i, i-1);
+                    allNodes[i].SetParentNode(allNodes[parentIndex]);
+                }
+                else
+                {
+                    baseNodes.Add(allNodes[i]);
+                }
+                
+            }
+
+        }
+
+        public int FindParent(int inputIndex, int compareIndex)
+        {
+            if (allNodes[inputIndex].depth <= allNodes[compareIndex].depth) // If This node is not a child (has less or equal depth) to the previous
+            {
+                return FindParent(inputIndex, compareIndex - 1);
+            }
+            else // If compareIndex is a parent
+            {
+                return compareIndex;
+            }
+
+        }
+
+        public string RemoveWhitespacesBeforeString(string s)
+        {
+            string outputString = "";
+            char[] tempCharArray = s.ToCharArray();
+
+            for (int j = 0; j < s.Count(); j++)
+            {
+                if (tempCharArray[j] != '\t' && tempCharArray[j] != ' ')
+                {
+                    outputString = outputString + tempCharArray[j];
+                }
+            }
+
+           return outputString;
+        }
+
+        public int CountWhitespacesBeforeString(string s)
+        {
+            int numberOfSpaces = 0;
+            char[] tempCharArray = s.ToCharArray();
+
+            for (int j = 0; j < s.Count(); j++)
+            {
+                if (tempCharArray[j] == '\t') //counts the number of tabs
+                {
+                    numberOfSpaces += 8;
+                }
+                else if (tempCharArray[j] == ' ')//counts the number of spaces
+                {
+                    numberOfSpaces += 1;
+                }
+                else // Instantly returns the number of spaces when getting a non whitespace character, in case someone has a space in their name
+                {
+                    return numberOfSpaces;
+                }
+
+            }
+            return numberOfSpaces;
+        }
+
+
+    }
+}
